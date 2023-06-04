26554f04ff0588a724c46f9e691d1118e2535c4c
Sophie Colette <69583383+SC-Git1@users.noreply.github.com>
2023-06-04 13:42:52 +0200

    Add files via upload

--- scripts/DatabaseMining/BacDive/main.py
+++ scripts/DatabaseMining/BacDive/main.py
@@ -3,11 +3,13 @@ from ncbi.datasets import TaxonomyApi
 from ncbi.datasets.openapi import ApiClient
 import time
 
+
 def get_lineage_from_taxid(taxid):
     rank_name = {}
 
     data = taxon_api.taxonomy_metadata(taxons=[taxid])
     list_lineage = data.get("taxonomy_nodes")[0].get("taxonomy").get("lineage")
+
     # remove the root
     list_lineage.remove(1)
     list_lineage.remove(131567)
@@ -30,18 +32,21 @@ def get_lineage_from_taxid(taxid):
 
 if __name__ == '__main__':
 
-    dfEntry = pd.read_csv("bacdive_entry_df.txt", delimiter="\t", header = 0)
-    # print(len(dfEntry)) -> there are initially 17707 records
+    dfEntry = pd.read_csv("bacdive_entry_df.csv", delimiter=",", header = 0)
+    # print(len(dfEntry)) # -> there are initially 17707 records
+
     # remove no or inconsistent growth data
     dfEntry = dfEntry[(dfEntry["growth"] != "no") & (dfEntry["growth"] != "inconsistent")]
-    # print(len(dfEntry)) -> 17463 records have a temperature annotation of type = optimum, growth, minimum or maximum
+    # print(len(dfEntry)) # -> 17463 records have a temperature annotation of type = optimum, growth, minimum or maximum
+
     # remove those without temperature annotation
     dfEntry = dfEntry[dfEntry['temperature'].notna()]
-    # print(len(dfEntry)) -> 17440 records have a valid, existing temperature annotation
-    print(dfEntry.groupby(['type', 'growth']).count())
+    # print(len(dfEntry)) # -> 17440 records have a valid, existing temperature annotation
+    # print(dfEntry.groupby(['type', 'growth']).count())
+
+    # remove duplicates i.e., points with the same source id, temperature value and temperature annotation type
     dfEntry.drop_duplicates(subset=["BacDiveID","type", "temperature"], inplace= True, ignore_index=True)
-    # print(len(dfEntry)) # 13273 records remain after removing duplicates i.e.,
-    # points with the same source id, temperature value and temperature annotation type
+    # print(len(dfEntry)) # 13277 records remain after removing duplicates
 
     ### Next, if optimum temperature annotation is available, keep those and discard other types for that BacDive ID
     #keep all rows of BacDiveIDs with optimum temperature available
@@ -50,7 +55,7 @@ if __name__ == '__main__':
     subset = subset[subset["type"] == "optimum"]
     # get all BacDiveIDs
     allIDs = list(dfEntry['BacDiveID'].unique())
-    # get all BacDiveIDs included in subset
+    # get all BacDiveIDs included in optimal subset
     subsetIDs = list(subset["BacDiveID"].unique())
     # find remaining IDs
     remainingIDs = list(set(allIDs).difference(set(subsetIDs)))
@@ -60,9 +65,11 @@ if __name__ == '__main__':
     dfNew = pd.concat([subset, remaining])
     dfNew = dfNew.sort_values(by=['BacDiveID'])
     dfNew.reset_index(inplace=True)
-    # print(len(dfNew)) -> there are 9534 remaining records
-    # print(len(Taxids.unique())) -> the records represent 4677 unique taxonomy IDs
+    # print(len(dfNew)) # -> there are 9543 remaining records
     Taxids = dfNew["id"]
+    # print(len(Taxids.unique()))  # -> the records represent 4677 unique taxonomy IDs
+
+    # prepare data
     Species = dfNew["name"]
     Temps = dfNew["temperature"]
     BacDiveIDs = dfNew["BacDiveID"]
@@ -71,18 +78,18 @@ if __name__ == '__main__':
     with ApiClient() as api_client:
         taxon_api = TaxonomyApi(api_client)
 
-    with open("NCBI_Annotated_BacDive2.tsv", "a") as f:
-        #f.write("Source_ID" + "\t" + "ncbiTaxID" + "\t" + "Name" + "\t" + "Type" + "\t" + "Temp" +
-        #        "\t" + "Superkingdom" + "\t" + "Phylum" + "\t" + "Class" + "\t" + "Order" + "\t" + "Family" + "\t" +
-        #        "Genus" + "\n")
-        for i in range(4995, len(Taxids)):
-            el = str(Taxids[i])
-            print(el)
+    with open("NCBI_Annotated_BacDive_Corr.tsv", "w") as f:
+        # header
+        f.write("Source_ID" + "\t" + "ncbiTaxID" + "\t" + "Name" + "\t" + "Type" + "\t" + "Temp" + "\t" + "Superkingdom"
+                + "\t" + "Phylum" + "\t" + "Class" + "\t" + "Order" + "\t" + "Family" + "\t" + "Genus" + "\n")
 
-            time.sleep(0.4)
+        for i in range(len(Taxids)):
+            el = str(Taxids[i])
+
+            time.sleep(0.3)
             rank_name = get_lineage_from_taxid(el)
+
             try:
-                print(rank_name)
                 Superkingdom = rank_name.get("SUPERKINGDOM")
                 if Superkingdom == None:
                     Superkingdom = ""
@@ -103,9 +110,8 @@ if __name__ == '__main__':
                     Genus = ""
             except:
                 pass
-            f.write(
-                str(BacDiveIDs[i]) + "\t" + el + "\t" + Species[i] + "\t" + Types[i] + "\t" + str(
-                    Temps[i]) + "\t" + Superkingdom + "\t"
-                + Phylum + "\t" + Class + "\t" + Order + "\t" + Family + "\t" + Genus + "\n")
+            # write to file
+            f.write(str(BacDiveIDs[i]) + "\t" + el + "\t" + Species[i] + "\t" + Types[i] + "\t" + str(Temps[i]) + "\t" +
+                    Superkingdom + "\t" + Phylum + "\t" + Class + "\t" + Order + "\t" + Family + "\t" + Genus + "\n")
             f.flush()
 

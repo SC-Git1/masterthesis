03d222e1e24e91bbd9fd4fbe210c208ec0ed643a
Sophie Colette <69583383+SC-Git1@users.noreply.github.com>
2023-06-04 13:51:08 +0200

    Add files via upload

--- scripts/DatabaseMining/CCUG/Lineage.py
+++ scripts/DatabaseMining/CCUG/Lineage.py
@@ -3,12 +3,14 @@ from ncbi.datasets import TaxonomyApi
 from ncbi.datasets.openapi import ApiClient
 import time
 
+
 def get_lineage_from_taxid(taxid):
 
     rank_name = {}
 
     data = taxon_api.taxonomy_metadata(taxons=[taxid])
     list_lineage = data.get("taxonomy_nodes")[0].get("taxonomy").get("lineage")
+
     # remove the root and the node "cellular organisms"
     list_lineage.remove(1)
     list_lineage.remove(131567)
@@ -33,6 +35,7 @@ if __name__ == "__main__":
     with ApiClient() as api_client:
         taxon_api = TaxonomyApi(api_client)
 
+    # import data
     dfCCUG = pd.read_csv("NCBITaxID_CCUG_man.tsv", delimiter="\t", header=0, index_col=False, encoding='latin1')
     # print(len(dfCCUG[dfCCUG["ncbiTaxID"] == -1]))  # 5 records do not haven an NCBI Taxonomy ID annotation
     dfCCUG = dfCCUG[dfCCUG["ncbiTaxID"] != -1]
@@ -46,14 +49,16 @@ if __name__ == "__main__":
 
     #SUPERKINGDOM, PHYLUM, CLASS, ORDER, FAMILY, GENUS
     with open("NCBI_Annotated_CCUG.tsv", "w") as f:
+        # header
         f.write("ncbiTaxID" + "\t" + "Source_ID" + "\t" + "Name" + "\t" + "Temp" + "\t" + "Superkingdom" + "\t" +
                 "Phylum" + "\t" + "Class" + "\t" + "Order" + "\t" + "Family" + "\t" + "Genus" + "\n")
+
         for i in range(len(Taxids)):
             el = str(Taxids[i])
-            print(el)
-           
+
             time.sleep(0.4)
             rank_name = get_lineage_from_taxid(el)
+
             try:
                 print(rank_name)
                 Superkingdom = rank_name.get("SUPERKINGDOM")
@@ -76,6 +81,8 @@ if __name__ == "__main__":
                     Genus = ""
             except:
                 pass
+
+            # write to file
             f.write(el + "\t" + str(Source_IDs[i]) + "\t" + Names[i] + "\t" + Temps[i] + "\t" + Superkingdom + "\t"
                 + Phylum + "\t" + Class + "\t" + Order + "\t" + Family  + "\t" + Genus + "\n")
             f.flush()
